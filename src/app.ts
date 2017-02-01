87ac56938f699c61e06e95ebd1b2deb865714957
Sachin Saluja <sachin.saluja132@gmail.com>
2017-02-01 17:13:33 +0100

    Added support for banner from query params

--- src/app.ts
+++ src/app.ts
@@ -240,12 +240,31 @@ export class App implements OnInit {
 
   public ngOnInit():void {
     this.reset();
+    const bannerLink = this.getParameterByName('bannerLink', '');
+    const bannerURL = this.getParameterByName('bannerURL', '');
+    if (bannerURL) {
+      this.signature['bannerEnabled'] = true;
+      this.signature['bannerLink'] = bannerLink;
+      this.signature['bannerURL'] = bannerURL;
+    }
   }
 
   public reset():void {
     this.signature = Object.assign({}, this.signatureModel);
     this.active = false;
     setTimeout(() => this.active = true, 0);
+  }
+
+  getParameterByName(name, url) {
+    if (!url) {
+      url = window.location.href;
+    }
+    name = name.replace(/[\[\]]/g, "\\$&");
+    let regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
   }
 }
 
